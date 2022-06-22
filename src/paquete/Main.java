3eca5244565253cc6386e38b5b15897a7fe3bc4e
Alex Betancur <alexbetbu@gmail.com>
2022-06-21 22:23:03 -0500

    Adding content to Class Person

--- src/paquete/Main.java
+++ src/paquete/Main.java
@@ -1,7 +1,22 @@
 package paquete;
+import paquete.Person;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        System.out.println("Welcome to Task 1");
+
+        Date d = new Date(1988, 7, 29);
+
+        Person p = new Person("Alex", "Betancur", "Builes", d);
+        p.setName("Juan");
+        p.setHeight(1.79f);
+
+        System.out.println("Full Name: " + p.getName() + " " + p.getLastName1() + " " + p.getLastName2());
+        System.out.println("Birthday: " + p.getDateBirth());
+        System.out.println("Height: " + p.getHeight());
+
+
     }
 }
--- src/paquete/Person.java
+++ src/paquete/Person.java
@@ -1,4 +1,57 @@
 package paquete;
+import java.util.Date;
 
 public class Person {
+    String name;
+    String lastName1;
+    String lastName2;
+    Date dateBirth;
+    float height;
+
+    public Person(String name, String lastName1, String lastName2, Date date){
+        this.name = name;
+        this.lastName1 = lastName1;
+        this.lastName2 = lastName2;
+        this.dateBirth = date;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setLastName1(String lastName1){
+        this.lastName1 = lastName1;
+    }
+
+    public String getLastName1(){
+        return this.lastName1;
+    }
+
+    public void setLastName2(String lastName2){
+        this.lastName2 = lastName2;
+    }
+
+    public String getLastName2(){
+        return this.lastName2;
+    }
+
+    public void setDateBirth(Date date){
+        this.dateBirth = date;
+    }
+
+    public Date getDateBirth(){
+        return this.dateBirth;
+    }
+
+    public void setHeight(float height){
+        this.height = height;
+    }
+
+    public float getHeight(){
+        return this.height;
+    }
 }
